eb9413325c91a272492a1b23c92f5a0c8d1dfe9d
MariaSchinoni <maria.schinoni10@gmail.com>
2022-12-02 12:02:11 -0300

    add Header component

---
+++ src/components/Header/Header.styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WrapperHeader = styled.header`
+  height: 20vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: var(--gray-700);
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { WrapperHeader } from "./Header.styles";
+
+export const Header = () => {
+  return (
+    <WrapperHeader>
+      <img src="src\assets\logo.svg" alt="logo" />
+    </WrapperHeader>
+  );
+};
